c224a322242f60d4a6da62a8bf72fb62a99ca6ac
Bruno Vieira <brunovieira439@gmail.com>
2022-06-25 05:36:07 -0300

    Aula 03

--- event-plataform/src/components/Lesson.tsx
+++ event-plataform/src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -9,27 +10,30 @@ interface LessonProps {
   type: 'live' | 'class';
 }
 
-export function Lesson({ title, avaliableAt, type }: LessonProps) {
+export function Lesson({ title, slug, avaliableAt, type }: LessonProps) {
   const isLessonAvaliable = isPast(avaliableAt);
   const avaliableDateFormatted = format(avaliableAt, 
     "EEEE' • 'd' de 'MMMM' • ' k'h'mm",
     {locale: ptBR});
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-grey-300">
         {avaliableDateFormatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 
+      group-hover:border-green-500">
 
         <header className="flex items-center justify-between">
         {isLessonAvaliable ? (
-          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+          <span className="text-sm text-blue-500 font-medium flex items-center
+          gap-2">
             <CheckCircle size={20} />
             Conteúdo Liberado
         </span>
         ) : (
-          <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+          <span className="text-sm text-orange-500 font-medium flex items-center
+          gap-2">
             <Lock size={20} />
             Em breve
           </span>
@@ -45,6 +49,6 @@ export function Lesson({ title, avaliableAt, type }: LessonProps) {
           {title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
